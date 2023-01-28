bb6f3beb3b3d086c69e80947b3f7a4dca281dfcb
Ensar Balık <ensarbalk@gmail.com>
2023-01-28 12:53:01 +0300

    Making "day"

--- Assets/Script/Sun/RealTimeSun.cs
+++ Assets/Script/Sun/RealTimeSun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,38 @@ using UnityEngine;
 public class RealTimeSun : MonoBehaviour
 {
     public Transform test;
+    float speed;
+
+    public float x;
+    public float y;
+    public float z;
+
+    public float currentTime;
+    public float dayLenghtMinutes;
+
+    private float rotationSpeed;
+    private float midday;
+    private float translateTime;
+
+    private void Start()
+    {
+        speed = 360 / dayLenghtMinutes / 60;
+        midday = dayLenghtMinutes * 60 / 2;
+    }
 
     void Update()
     {
-        transform.LookAt(test);
+        //transform.LookAt(test);
+        //transform.RotateAround(test.position, new Vector3(x, y, z), speed * Time.deltaTime);
+
+        currentTime += 1 * Time.deltaTime;
+
+        transform.Rotate(new Vector3(1, 0, 0) * (speed * Time.deltaTime));
+
+        float t = translateTime * 24f;
+
+        float hours = Mathf.Floor(t);
+
+        Debug.Log(hours);
     }
 }
